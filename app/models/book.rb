fff667e717bffa8d53e0e182de7396164957ed51
Mai Duy Anh <maiduyanh@Duy-Anhs-MBP.lan>
2023-04-05 07:56:29 +0700

    Add model validation and association

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,10 @@
 class Book < ApplicationRecord
   belongs_to :subject
+validates_presence_of :title, :author, :publisher
+validates_uniqueness_of :title
+validates_numericality_of :year
+validate :year_up_to_present
+def year_up_to_present
+errors.add(:field_name, 'Year must not be in the future') if year >
+Time.now.year
 end
