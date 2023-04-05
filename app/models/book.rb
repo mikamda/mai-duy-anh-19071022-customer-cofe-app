e9cb164e0f73f044b6b24966349d833612deefe3
Mai Duy Anh <maiduyanh@Duy-Anhs-MBP.lan>
2023-04-05 08:30:50 +0700

    Add rails admin

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,4 @@ validates_presence_of :title, :author, :publisher
 validates_uniqueness_of :title
 validates_numericality_of :year
 validate :year_up_to_present
-def year_up_to_present
-errors.add(:field_name, 'Year must not be in the future') if year >
-Time.now.year
 end
